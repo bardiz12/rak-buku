003ce5e17a26a731e4a5331e771c99ea9357ea28
Bardizba <bardizba@mifx.com>
2022-11-08 03:23:58 +0700

    update: index page

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(__dirname + '/../data/db.json',)
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router(__dirname + '/../data/db.json')
+const middlewares = jsonServer.defaults({
+    readOnly: true
+})
 
 server.use(middlewares)
-server.use('/api', router)
+server.route('/api').get((req, res) => {
+    res.send({
+        booksApi: '/api/books'
+    })
+})
+
+server.route('/').get((req, res) => {
+    res.send({
+        api: '/api',
+        booksApi: '/api/books'
+    })
+})
 
 module.exports = server;
