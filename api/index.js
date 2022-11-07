129d1c99c35a9fc8a3fe7154526ec0752dae809f
Bardizba <bardizba@mifx.com>
2022-11-08 02:49:47 +0700

    first init

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(__dirname + '/../data/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+module.exports = server;
---
+++ server.js
@@ -0,0 +1,5 @@
+const server = require('./api')
+
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
