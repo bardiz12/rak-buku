3f57753124f23f361d992ee7e1e290e83cc10556
Bardizba <bardizba@mifx.com>
2022-11-08 03:06:24 +0700

    update: prefix url

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(__dirname + '/../data/db.json')
+const router = jsonServer.router(__dirname + '/../data/db.json',)
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(router)
+server.use('/api', router)
 
 module.exports = server;
