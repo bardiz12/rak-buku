2214be5b04bf3937deee2e4f151752ede32973cb
Bardizba <bardizba@mifx.com>
2022-11-08 03:57:28 +0700

    use express

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router(__dirname + '/../data/db.json')
-const middlewares = jsonServer.defaults({
-    readOnly: true
-})
 
-server.use(middlewares)
-server.route('/api').get((req, res) => {
-    res.send({
-        booksApi: '/api/books'
+const allowedMethods = ['GET', 'HEAD']
+
+server.use((req, res, next) => {
+    if (!allowedMethods.includes(req.method)) return res.status(405).send({
+        message: 'method not allowed'
     })
+    return next()
 })
 
-server.route('/').get((req, res) => {
+server.use('/api', router)
+
+server.get('/', (req, res) => {
     res.send({
-        api: '/api',
         booksApi: '/api/books'
     })
 })
